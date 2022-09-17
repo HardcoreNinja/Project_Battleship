e81178f1de0b1ecf0ce24355da10268ab4d8de26
anonymous_pc <anonymous_pc_serbia@protonmail.com>
2022-09-17 16:59:18 -0400

    Cleaned up the BattleShip class to get rid of all the unnecessary Coordinate stuff.
    Cleaned up the BattleShip class to get rid of all the unnecessary Coordinate stuff.

--- src/test/battleship.js
+++ src/test/battleship.js
@@ -2,28 +2,11 @@ class BattleShip {
   constructor(name, length) {
     this.name = name;
     this.length = length;
-    this.coordinateArray = Array(length).fill(null);
     this.horizontalVertical = true;
   }
 
   changeOrientation() {
     this.horizontalVertical = !this.horizontalVertical;
-  }
-
-  setCoordinates(coordinate) {
-    for (let i = 0; i < this.coordinateArray.length; i++) {
-      if (this.horizontalVertical) {
-        this.coordinateArray[i] = [coordinate.coordinate[0] + i,
-          coordinate.coordinate[1]];
-      } else {
-        this.coordinateArray[i] = [coordinate.coordinate[0],
-          coordinate.coordinate[1] + i];
-      }
-    }
-  }
-
-  clearCoordinates() {
-    for (let i = 0; i < this.coordinateArray.length; i++) { this.coordinateArray[i] = null; }
   }
 }
 
--- src/test/battleship.test.js
+++ src/test/battleship.test.js
@@ -1,5 +1,5 @@
 const BattleShip = require('./battleship');
-const Coordinate = require('./coordinate');
+// const Coordinate = require('./coordinate');
 
 test('length | WORKS!', () => {
   expect(new BattleShip('Destroyer', 3).length).toBe(3);
@@ -12,56 +12,11 @@ test('horizontalVertical && changeOrientation() | WORKS!', () => {
   expect(battleship.horizontalVertical).toBe(false);
 });
 
-test('getcoordinateArray| WORKS for INVALID (RETURNS NULL ARRAY)', () => {
-  expect(new BattleShip('Destroyer', 3).coordinateArray).toStrictEqual([null, null, null]);
-});
-
-test('setCoorindates()| WORKS for INVALID (RETURNS NULL ARRAY)', () => {
-  const battleship = new BattleShip('Destroyer', 3);
-  battleship.setCoordinates(new Coordinate([0, 0]));
-  expect(battleship.coordinateArray).toStrictEqual(
-    [
-      [0, 0],
-      [1, 0],
-      [2, 0],
-    ],
-  );
-});
-
-test('clearCoordinates()| WORKS', () => {
-  const battleship = new BattleShip('Destroyer', 3);
-  battleship.setCoordinates(new Coordinate([0, 0]));
-  expect(battleship.coordinateArray).toStrictEqual(
-    [
-      [0, 0],
-      [1, 0],
-      [2, 0],
-    ],
-  );
-  battleship.clearCoordinates();
-  expect(battleship.coordinateArray).toStrictEqual([null, null, null]);
-});
-
 test('changeOrientation()| WORKS for BOTH Horiztonal & Vertical Orientation', () => {
   const battleship = new BattleShip('Destroyer', 3);
-  battleship.setCoordinates(new Coordinate([0, 0]));
-  expect(battleship.coordinateArray).toStrictEqual(
-    [
-      [0, 0],
-      [1, 0],
-      [2, 0],
-    ],
-  );
-  battleship.clearCoordinates();
+  expect(battleship.horizontalVertical).toBe(true);
   battleship.changeOrientation();
-  battleship.setCoordinates(new Coordinate([0, 0]));
-  expect(battleship.coordinateArray).toStrictEqual(
-    [
-      [0, 0],
-      [0, 1],
-      [0, 2],
-    ],
-  );
+  expect(battleship.horizontalVertical).toBe(false);
 });
 
 test('name', () => {
