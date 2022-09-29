0f1122abe80184894b249c6e370d7e323add1b9c
anonymous_pc <anonymous_pc_serbia@protonmail.com>
2022-09-29 13:30:48 -0400

    Attempt to fix favicon issue on github pages.
    Attempt to fix favicon issue on github pages.

--- src/head/head.js
+++ src/head/head.js
@@ -4,7 +4,7 @@ function createFavicon() {
   const head = document.querySelector('head');
   const link = document.createElement('link');
   link.rel = 'shortcut icon';
-  link.href = favicon;
+  link.href = `${favicon}?`;
   link.type = 'image/x-icon';
   head.append(link);
 }
